4791ec784bb110e253f6fba8e1593a661bc564a4
ABHIRAM_S <149877367+Abhiram-ARS@users.noreply.github.com>
2025-05-01 14:22:56 +0530

    Update weatherApp.js

--- Weather_App_using_API/weatherApp.js
+++ Weather_App_using_API/weatherApp.js
@@ -5,7 +5,6 @@ async function getWeather(city){
     try{
         const API_KEY = process.env.API_KEY
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        console.log(url)
         const response = await axious.get(url)
         const data = response.data
 
